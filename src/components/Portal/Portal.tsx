175848f7ee50ba9517f2694894e734f4589b9dc5
Nick <chidojiro@gmail.com>
2022-12-18 17:03:15 +0700

    feat: Portal component

---
+++ src/components/Portal/Portal.tsx
@@ -0,0 +1,10 @@
+import { createPortal } from 'react-dom';
+import { Children } from '@/types';
+
+export type PortalProps = Children & {
+  asChildOf?: HTMLElement | null;
+};
+
+export const Portal = ({ children, asChildOf }: PortalProps) => {
+  return createPortal(children, asChildOf ?? document.getElementById('root') ?? document.body);
+};
