4aead0be48fa3409b83c8db20a8d400cfece2cfa
Nick <chidojiro@gmail.com>
2022-12-18 16:26:42 +0700

    feat: Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
+import { Merge } from '@/types';
+import clsx from 'clsx';
 import type { ReactNode } from 'react';
 
-export type NstButtonProps = {
-  /**
-   * Children is required on a button, at least either an icon or text.
-   */
-  children: ReactNode;
-};
+export type NstButtonProps = Merge<
+  JSX.IntrinsicElements['button'],
+  {
+    /**
+     * Children is required on a button, at least either an icon or text.
+     */
+    children: ReactNode;
+  }
+>;
 
 /**
  * This is a simple example of a button 🖱. Let's try to make it satisfy all the requirements we stated in the design system.
@@ -13,11 +18,20 @@ export type NstButtonProps = {
  * You likely will run into a classic Tailwind problem here 😑.
  * >Feel free to use 3rd party library like [tailwind-merge](https://github.com/dcastil/tailwind-merge) if you want.
  */
-export const NstButton = (props: NstButtonProps) => {
+export const NstButton = ({ className, type = 'button', ...restProps }: NstButtonProps) => {
   return (
     <button
-      className="text-white px-4 py-2 bg-rose-500 shadow-sm border-rose-500 rounded-lg"
-      {...props}
+      type={type}
+      className={clsx(
+        'focusable',
+        'px-4 py-2',
+        'text-white',
+        'bg-rose-500',
+        'border-rose-500 rounded-lg',
+        'shadow-sm',
+        className,
+      )}
+      {...restProps}
     />
   );
 };
